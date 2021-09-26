0bb81291c9249a9384444689e8aea07821bc05be
mohamedmagdy007 <mohamed.mo9751@gmail.com>
2021-09-26 21:38:39 +0200

    edit navbar&scrollTop

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,4 +1,20 @@
-function myFunction(){
-    var navber = document.querySelector(".navbar-collapse");
+let navber = document.querySelector(".navbar-collapse");
+let toggle = document.querySelector(".navbar-toggle");
+let span = document.querySelector('.up');
+toggle.onclick = function(){
     navber.classList.toggle("close");
+}
+window.onscroll= function(){
+    if(this.scrollY >=500){
+        span.classList.add("show")
+    }
+    else{
+        span.classList.remove("show")
+    }
+}
+span.onclick =function(){
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    })
 }
